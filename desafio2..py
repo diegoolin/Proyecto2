d8f3768003ff13124ee18329a38afad8a8df593d
diegoolin <112409168+diegoolin@users.noreply.github.com>
2022-10-23 23:48:33 -0300

    Add files via upload

---
+++ desafio2..py
@@ -0,0 +1,99 @@
+n=int(input("ingrese filas:"))
+m=int(input("ingrese columnas:"))
+lista=[]
+for i in range(n):
+	fila = []
+	for j in range(m):
+		fila.append(0)
+	lista.append (fila)
+
+for i in range(n):
+    for j in range (m):
+        print("|",lista[i][j], "|", end=" ")
+    print()
+
+menu = int(input("1 para ubicar un contenedor \n2 para ingresar el contenedor \n3 para retirar un contenedor  \n4 para terminar la jornada laboral \nIngrese opción:"))
+while menu !=4:
+    if menu == 1:
+        x=0
+        numerocontenedor= int(input("ingrese el numero del contenedor para ubicarlo: "))     
+        nombreempresa = input("ingrese el nombre de la empresa:")
+        if lista(n,m)==numerocontenedor:
+            print(n,m)
+            x=1
+
+        seguir = input("¿desea continuar?:")
+        while seguir== "si":
+            numero = int(input("ingrese el numero del contenedor: "))
+            nombreempresa = input("ingrese el nombre de la empresa propietaria: ")
+            seguir = input("¿desea continuar?:")
+           
+        menu = int(input("1 para ubicar un contenedor \n2 para ingresar un contenedor \n3 para retirar un contenedor  \n4 para terminar la jornada \neleccion:"))
+
+    if menu == 2:
+        
+        numerocontenedor=input("Ingrese numero del contenedor a añadir:")
+        nombreempresa = input("Ingrese nombre de la empresa:")
+        ubicacionf= int(input("Ingrese a que fila desea ingresar el contenedor:"))
+        ubicacionc= int(input("Ingrese a que columna desea ingresar el contenedor:")) 
+
+        lista[ubicacionf][ubicacionc] = numerocontenedor+"/"+nombreempresa
+
+        for i in range(n):
+            for j in range (m):
+                print("|",lista[i][j],"|",end=" ")
+            print()
+        
+        seguir= input("¿desea continuar?: ")
+        while seguir== "si":
+            numerocontenedor=input("Ingrese numero del contenedor a añadir:")
+            nombreempresa = input("Ingrese nombre de la empresa:")
+            ubicacionf= int(input("Ingrese a que fila desea ingresar el contenedor:"))
+            ubicacionc= int(input("Ingrese a que columna desea ingresar el contenedor:"))
+
+            lista[ubicacionf][ubicacionc] = numerocontenedor+"/"+nombreempresa
+
+        
+            for i in range(n):
+                for j in range (m):
+                    print("|",lista[i][j],"|",end=" ")
+                print()
+            seguir = input("¿desea continuar?:")
+
+        menu = int(input("1 para ubicar un contenedor \n2 para ingresar un contenedor \n3 para retirar un contenedor  \n4 para terminar la jornada laboral \nIngrese opción:"))
+
+    if menu == 3:
+        numerocontenedor=int(input("Ingrese numero del contenedor a retirar:"))
+        nombreempresa = input("Ingrese nombre de empresa:")
+
+        if lista[ubicacionf-1][ubicacionc] == None:
+            lista[ubicacionf][ubicacionc] = None
+        else:
+            print("hay un contenedor arriba por lo que el contenedor no se puede retirar")
+
+       
+        for i in range(n):
+            for j in range (m):
+                print("|", lista[i][j], "|", end=" ")
+            print()
+        seguir = input("¿desea continuar?:")
+        while seguir == "si":
+            numerocontenedor=int(input("Ingrese numero del contenedor a retirar:"))
+            empresa = input("Ingrese nombre de empresa:")
+
+            if lista[ubicacionf-1][ubicacionc] == 0:
+                lista[ubicacionf][ubicacionc] = 0
+            else:
+                print("hay un contenedor arriba por lo que el contenedor no se puede retirar")
+
+        
+            for i in range(n):
+                for j in range (m):
+                    print("|", lista[i][j], "|", end=" ")
+                print()
+                seguir= input("¿desea continuar?:")
+            seguir = input("¿desea continuar?:")
+        
+        menu = int(input("1 para ubicar un contenedor \n2 para ingresar un contenedor \n3 para retirar un contenedor  \n4 para terminar la jornada \neleccion:"))
+        
+print("Gracias termino el periodo laboral")
